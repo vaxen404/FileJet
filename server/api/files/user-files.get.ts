b17bf6e89202a4e36da007bd14aa039158abf19f
vaxen404 <vaxen404@gmail.com>
2026-05-08 21:54:55 +0300

    Update file path to use uploads directory

--- server/api/files/user-files.get.ts
+++ server/api/files/user-files.get.ts
@@ -5,7 +5,7 @@ export default defineEventHandler(async (event) => {
     const query = getQuery(event)
     const username = query.username
 
-    const DB_PATH = join(process.cwd(), 'data', 'users.json')
+    const storageDir = join(process.cwd(), 'public', 'uploads')
 
     try{
         const data = await fs.readFile(DB_PATH, 'utf-8')
